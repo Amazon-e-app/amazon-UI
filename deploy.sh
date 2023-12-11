f20c8e604959d9bd7f618dfe785a3dca560f9bcb
sri455 <srinu.banoth04@gmail.com>
2023-12-11 10:01:06 +0000

    addded fils

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-
-echo "Hello Guys"
-echo "Welcome to ShellScript"
-echo "This is the first shell script example !!"
-echo "Today date is"
-date
-echo "Shellscript is very easy"
-
-
--- student.java
+++ student.java
@@ -1,26 +1,2 @@
-package com.java.examples;
-
-public class ArrayExp 
-{
-
-	public static void main(String[] args) 
-	{
-		int a[] = {10,20,30,40,50,60};
-		System.out.println("Length of array a[] is "+a.length);
-		for(int i=0;i<a.length;i++)
-		{
-			System.out.println("i is "+a[i]);
-		}
-		String data = "I am learning 343 (*&";
-		for(int i = 0;i<data.length();i++)
-		{
-			System.out.print(data.charAt(i));
-			char ch = data.charAt(i);
-			System.out.println(Character.isAlphabetic(ch));
-		}
-		String wd[] = data.split(" ");
-		System.out.println("Number of words in the string are "+wd.length);
-		
-	}
-
-}
+Hello guys 
+i am a softwear tester
